d09544250b8e6abbf40bffa9ecef8d6c928fd53d
mykyttem <nrabicenko5@gmail.com>
2025-04-11 13:53:09 +0300

    pref(backend firebase_config) get from etc json

--- backend/app/services/firebase/firebase_config.py
+++ backend/app/services/firebase/firebase_config.py
@@ -2,19 +2,13 @@ import os
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Get the current directory
-current_dir = os.path.dirname(os.path.abspath(__file__))
+cert_path = '/etc/secrets/crf.json'
 
-# Path to the certificate
-cert_path = os.path.join(current_dir, "crf.json")
-
-# Check if the file exists
 if not os.path.exists(cert_path):
     print(f"file '{cert_path}' not found!")
     exit(1)
 
 try:
-    # Initializing Firebase
     cred = credentials.Certificate(cert_path)
     firebase_admin.initialize_app(cred)
 
